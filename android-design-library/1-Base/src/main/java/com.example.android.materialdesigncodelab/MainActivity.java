fc8c5524bb6e790186f9e4a42115327a4229c7a4
AlexKatrenich <alex.katrenich@gmail.com>
2018-06-26 10:30:53 +0300

    changed theme style

--- android-design-library/1-Base/src/main/java/com.example.android.materialdesigncodelab/MainActivity.java
+++ android-design-library/1-Base/src/main/java/com.example.android.materialdesigncodelab/MainActivity.java
@@ -18,7 +18,6 @@ package com.example.android.materialdesigncodelab;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
