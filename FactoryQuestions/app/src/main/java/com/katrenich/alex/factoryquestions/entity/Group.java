319097e6e91ea730b2d16e515869ca8e7672d2ed
AlexKatrenich <alex.katrenich@gmail.com>
2018-07-21 22:43:56 +0300

    added entities

---
+++ FactoryQuestions/app/src/main/java/com/katrenich/alex/factoryquestions/entity/Group.java
@@ -0,0 +1,11 @@
+package com.katrenich.alex.factoryquestions.entity;
+
+import java.util.List;
+
+public class Group {
+    private int groupId;
+    private String groupName;
+    private List<User> users;
+//    private List<Questionary> questionaries;
+
+}
---
+++ FactoryQuestions/app/src/main/java/com/katrenich/alex/factoryquestions/entity/User.java
@@ -0,0 +1,10 @@
+package com.katrenich.alex.factoryquestions.entity;
+
+public class User {
+    private String fullName;
+    private int userId;
+    private String hash;
+    private String email;
+
+
+}
