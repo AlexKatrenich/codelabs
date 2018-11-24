ca3f836b4ae26ac47ae576a91dc75a2e2808bf62
AlexKatrenich <alex.katrenich@gmail.com>
2018-11-24 15:32:41 +0200

    Add styles to AppBarLayout'

--- FactoryQuestions/app/src/main/java/com/katrenich/alex/factoryquestions/activities/QuestionsActivity.java
+++ FactoryQuestions/app/src/main/java/com/katrenich/alex/factoryquestions/activities/QuestionsActivity.java
@@ -2,22 +2,50 @@ package com.katrenich.alex.factoryquestions.activities;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
 import com.katrenich.alex.factoryquestions.R;
 
 public class QuestionsActivity extends BaseActivity {
+    private final String TAG = "QuestionsActivity_";
+    Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // Ініціалізація полів екрану та змінних
+        init();
+    }
+
+    private void init() {
+        /*Ініціалізація верхнього меню*/
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(false);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
-        getSupportActionBar().setTitle("Questions list");
+
+        getSupportActionBar().setTitle(
+                getResources().getString(
+                        R.string.questions_list_activity_title));
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                onBackPressed();
+                Log.d(TAG, "init: mToolbar -> onBackPressed()");
+            }
+        });
+
+
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
